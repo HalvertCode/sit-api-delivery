c81350eed4f7b919c3bec7545c8ca7deb3efec8f
Halvert <hguerrerop1@upao.edu.pe>
2023-04-20 15:39:46 -0500

    feat: Crear clase OrderService para manejar la logica de negocio de los pedidos

---
+++ api-delivery/src/main/java/com/delivery/apidelivery/services/OrderService.java
@@ -0,0 +1,34 @@
+package com.delivery.apidelivery.services;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+import com.delivery.apidelivery.entities.Order;
+public class OrderService {
+    private Map<Integer, Order> orders = new HashMap<>();
+    private int nextId = 1;
+
+    public List<Order> getAllOrders() {
+        return new ArrayList<>(orders.values());
+    }
+
+    public Order getOrderById(int id) {
+        return orders.get(id);
+    }
+
+    public Order createOrder(Order order) {
+        order.setId(nextId++);
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    public Order updateOrder(Order order) {
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    public void deleteOrder(int id) {
+        orders.remove(id);
+    }
+}
